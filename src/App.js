e15392623eac35db594200ddbb99b09b5f76330d
Roba Nath Basnet <robanbth@gmail.com>
2022-10-26 02:53:02 +0600

    All setup done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="text-5xl font-extrabold ...">
+          <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+            Music Player
+          </span>
+        </div>
+        <h1 className="text-2xl font-bold text-green-500 mt-4">
+          Coming soon...
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
